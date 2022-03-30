ec1d548b9d3d74c019c67050eb17d713e3335db2
liteodoro <teodorolilisilva@gmail.com>
2022-03-30 12:44:05 -0300

    ajustando

---
+++ src/components/pages/MulherView.vue
@@ -0,0 +1,183 @@
+<template>
+    <main id="home">
+    <div class="btnVoltar">
+        <a href="#home"><i class="bi bi-arrow-up-circle"></i></a>
+    </div>
+    
+      <navbar-page />
+
+         <section id="palalogin" class="container text-white bg-green">
+            <h3>Costela de Adão Modas</h3>
+            
+         </section>
+
+         <section id="corpo" class=" container bg-whitesmoke">
+             <div class="p-5">
+                 <h2>Artigos Femininos</h2>
+             </div>
+
+            <div class="row">
+                <div class="col-md-3 col-12">
+                  <div class="row produtos">
+                      <div>
+                         <img src="@/assets/imagens/girl1.jpg" alt="Produto 1" class="img-fluid">
+                      </div>
+                      <div class="p-1">
+                          <h2>Produto 1</h2>
+                      </div>
+                      <div class="p-1">
+                        <p>Descrição produtos</p>
+                      </div>
+                      <div class="p-1">
+                        <p>Valor</p>
+                      </div>
+                      <div class="col-6">
+                        <input type="number" step="1" min="0" max="10" value="0" class="col-12">
+                      </div>
+                      <div class="col-6">
+                        <div class="">
+                          <a href="produto-detalhe.html" target="_blank">
+                              <button type="button" class="col-12 bg-yellow">Comprar</button>
+                          </a>
+                       </div>
+                      </div>
+                  </div>
+               </div>
+                    
+                <div class="col-md-3 col-12">
+                  <div class="row produtos ">
+                      <div>
+                          <img src="@/assets/imagens/girl2.jpg" alt="Produto 1" class="img-fluid">
+                      </div>
+                      <div class="p-1">
+                          <h2>Produto 2</h2>
+                      </div>
+                      <div class="p-1">
+                          <p>Descrição produtos</p>
+                      </div>
+                      <div class="p-1">
+                          <p>Valor</p>
+                      </div>
+                      <div class="col-6">
+                          <input type="number" step="1" min="0" max="10" value="0" class="col-12">
+                      </div>
+                      <div class="col-6">
+                          <button type="button" class="col-12 bg-yellow">Comprar</button>
+                      </div>
+                  </div>
+               </div>
+
+              <div class="col-md-3 col-12">
+                <div class="row produtos">
+                    <div>
+                        <img src="@/assets/imagens/girl3.jpg" alt="Produto 1" class="img-fluid">
+                    </div>
+                    <div class="p-1">
+                        <h2>Produto 3</h2>
+                    </div>
+                    <div class="p-1">
+                        <p>Descrição produtos</p>
+                    </div>
+                    <div class="p-1">
+                        <p>Valor</p>
+                    </div>
+                    <div class="col-6">
+                        <input type="number" step="1" min="0" max="10" value="0" class="col-12">
+                    </div>
+                    <div class="col-6">
+                        <button type="button" class="col-12 bg-yellow">Comprar</button>
+                    </div>
+                </div>
+             </div>
+             <div class="col-md-3 col-12">
+                <div class="row produtos">
+                    <div>
+                        <img src="@/assets/imagens/girl4.jpg" alt="Produto 1" class="img-fluid">
+                    </div>
+                    <div class="p-1">
+                        <h2>Produto 4</h2>
+                    </div>
+                    <div class="p-1">
+                        <p>Descrição produtos</p>
+                    </div>
+                    <div class="p-1">
+                        <p>Valor</p>
+                    </div>
+                    <div class="col-6">
+                        <input type="number" step="1" min="0" max="10" value="0" class="col-12">
+                    </div>
+                    <div class="col-6">
+                        <button type="button" class="col-12 bg-yellow">Comprar</button>
+                    </div>
+                </div>
+            </div>
+          </div>
+                   
+         </section>
+         <section id="sugestao" class="container text-center text-white bg-orangered">
+             <h2>Sugestões</h2>
+
+             <div class="row row-cols-1 row-cols-md-3 g-4">
+              <div class="col">
+                <div class="card h-100">
+                  <img src="@/assets/imagens/Chapeu.png" class="card-img-top" alt="sugestao1">
+                  <div class="card-body text-black">
+                    <h5 class="card-title text-black">Chapéus</h5>
+                    <p class="card-text">Chapéu Vintage Moda Panamá Palha Shantung.</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Promoção valida do dia 31/01 até dia 5/02</small>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <img src="@/assets/imagens/Bolsa.jpg" class="card-img-top" alt="sugestao1">
+                  <div class="card-body">
+                    <h5 class="card-title text-black">Bolsas</h5>
+                    <p class="card-text text-black">Cabe tudo: as suas companheiras de rotina. Entre os modelos de bolsas que vão e voltam, estão as bolsas da moda</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Promoção valida do dia 31/01 até dia 5/02</small>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <img src="@/assets/imagens/acessorios.jpg" class="card-img-top" alt="sugestao1">
+                  <div class="card-body text-black">
+                    <h5 class="card-title">Acessórios</h5>
+                    <p class="card-text text-black">óculos e outros acessórios pra completar seu look.</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">Promoção valida do dia 31/01 até dia 5/02</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+         </section>
+
+         <footer class="container text-end bg-white">
+          <p> Projeto Aline Teodoro </p>
+         </footer>
+
+  </main>     
+</template>
+
+<script>
+export default {
+  name: "MulherView.vue",
+};
+
+import NavbarPage from "@/components/pages/NavbarPage.vue";
+
+export default {
+  components: {
+    NavbarPage,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/pages/ProdutodetView.vue
@@ -0,0 +1,161 @@
+<template>
+    <main id="home">
+    <div class="btnVoltar">
+        <a href="#home"><i class="bi bi-arrow-up-circle"></i></a>
+    </div> 
+    
+         <navbar-page />
+    
+         <section id="palalogin" class="container text-white bg-green">
+            <h3>Costela de Adão Modas</h3>
+            
+         </section>
+
+        <section id="detalhe" class="container bg-whitesmoke">
+
+            <div class="p-2 text-center">
+                <h2>Descrição </h2>
+            </div>
+               
+            <div class="container">
+                <div class="row">
+                  <div class="col-md-9 col-12 py-3">
+                      <h3>Vestido godê</h3>
+
+                  </div>
+                  <div class="col-md-4 col-12">
+                    <div class="card" style="width: 18rem;">
+                        <img src="@/assets/imagens/girl1.jpg" class="card-img-top" alt="img-fluid">
+                      </div>
+
+                  </div>
+                  <div class="col-md-6 col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="p-2 text-end fs-5">
+                                <p>Avaliação</p>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-half"></i>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="text-start py-2 px-4">
+                                <h3>Vestido Estampa lisa de Alças</h3>
+
+                            </div>
+                            <div class="text-black py-2 px-4 fw-bold">
+                                <p>R$ 150</p>
+                                <p>Ou até 3x 50,00 no cartão</p>
+                            </div>
+                            <div class="row"> 
+                                <div class="col-md-2">
+                                    <input type="number" step="1" min="0" max="10" value="0" class="col-12">
+                                </div>
+                                <div class="col-md-3">
+                                    <button type="button" class="col-12 bg-yellow">Comprar</button>
+                                </div>
+                                <div class="row">
+                                        <div class="btn-toolbar justify-content-between px-3 py-5" role="toolbar" aria-label="Toolbar with button groups">
+                                           <div class="btn-group" role="group" aria-label="First group">
+                                               <button type="button" class="btn btn-outline-secondary">pp</button>
+                                               <button type="button" class="btn btn-outline-secondary">p</button>
+                                               <button type="button" class="btn btn-outline-secondary">M</button>
+                                               <button type="button" class="btn btn-outline-secondary">G</button>
+                                               <button type="button" class="btn btn-outline-secondary">GG</button>
+                                           </div>
+                                        </div>
+                                </div>
+                            </div>
+
+                        </div>
+                      </div>
+                </div>
+                </div>
+              </div>
+              <div class="row">
+                  <div class=" fw-bold py-5 px-2">
+                      <H3>Descrição Detalhada:</h3>
+
+                        <div class="p-4">
+                          <p>
+                            O Vestido Curto Jeans Plus Size Bufante Uma Maria Denim Claro Filipa by Costela de Adão modas é um visual <br>
+                            completo quando você busca por praticidade e conforto, mas sem deixar o estilo de lado! Aposte!<br>
+
+                            <p class="py-3 px-2">
+                              <h4> Características:</h4>
+                               Jeans claro
+                               Comprimento curto
+                               Decote V
+                               Babado maria na barra
+                               Aplicação de botões frontais
+                            <p>
+                          </p>
+                        </div>
+                    </div>
+              </div>
+              <div class="fw-bold py-5">
+                  <h2>Avaliação</h2>
+
+              </div>
+              <div>
+                <div class="row">
+                    <div class="col-md-8 col-12 py-2 px-5">
+                        <div class="row produtos p-2 border-circle">
+                            <div class="col-md-4 col-12 text-center">
+                                <img src="@/assets/imagens/girl1.jpg" class="img-fluid rounded-circle">
+                            </div>
+                            <div class="col-md-8 col-12 pt-4">
+                                <div class="border-bottom border-dark">
+                                    <h2>Amei!!</h2>
+                                    <p>Ficou bonito, mas ficou um pouco justo no corpo! O produto e de ótima qualidade. </p>
+                                </div>
+                                <div class="p-2 text-end fs-3">
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-half"></i>
+                                    <i class="bi bi-emoji-heart-eyes"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-12 py-2 px-5">
+                      <div class="row produtos p-2 border-circle">
+                          <div class="col-4">
+                              <img src="@/assets/imagens/girl1.jpg" class="img-fluid rounded-circle">
+                          </div>
+                          <div class="col-md-8 col-12 pt-4">
+                              <div class="border-bottom border-dark">
+                                  <h2>Adorei!!</h2>
+                                  <p>O vestido ficou super bonito!! O pano e de ótima Qualidade, ficou perfeito! compraria de novo!</p>
+                              </div>
+                              <div class="p-2 text-end fs-3">
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-star-fill"></i>
+                                <i class="bi bi-emoji-heart-eyes"></i>
+                              </div>
+                          </div>
+                      </div>
+                    </div>
+              </div>
+        </section>
+        <footer class="container text-end bg-white">
+            <p>Projeto Aline Teodoro</p>
+        </footer>
+    </main>
+</template>
+
+<script>
+export default {
+  name: "ProdutodetView.vue",
+};
+
+import NavbarPage from "@/components/pages/NavbarPage.vue";
+
+export default {
+  components: {
+    NavbarPage,
+  },
+};
+</script>
